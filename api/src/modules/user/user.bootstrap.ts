ea97f18a610be5499f5c0d935af646897a23a3e1
Bambino <boon.zaza55@gmail.com>
2024-04-17 18:09:11 +0700

    update

--- api/src/modules/user/user.bootstrap.ts
+++ api/src/modules/user/user.bootstrap.ts
@@ -1,6 +1,8 @@
 import { UserController } from "./user.controller";
 import UserModel from "./user.schema";
 import { UserRepository } from "./user.repository";
+import { TypedRoute } from "@authorization-provider/core";
 
+export const route = new TypedRoute()
 const userRepository = new UserRepository(UserModel)
 export const userController = new UserController(userRepository)
---
+++ core/src/typed-routes.ts
@@ -0,0 +1,100 @@
+import { z } from 'zod';
+import { MaybePromise } from './types';
+import { BaseResponse } from './responses';
+import { Request, Response } from 'express';
+import { fromZodError } from 'zod-validation-error';
+import { ValidationError } from './errors';
+import { RequestHandler } from './router';
+
+export type TypedHandle<
+  TQuery extends z.ZodTypeAny,
+  TParams extends z.ZodTypeAny,
+  TBody extends z.ZodTypeAny,
+  TResponse extends BaseResponse = BaseResponse
+> = (context: {
+  query: z.infer<TQuery>;
+  params: z.infer<TParams>;
+  body: z.infer<TBody>;
+  req: Request<z.infer<TParams>, z.infer<TBody>, z.infer<TQuery>>;
+  res: Response<TResponse>
+}) => MaybePromise<TResponse>;
+
+export interface HandlerMetadata {
+  __handlerMetadata: true;
+  method: string;
+  path: string;
+  handler: RequestHandler;
+}
+
+export class TypedRoute {
+  get(path: string) {
+    return new TypedRouteHandler(path, 'get');
+  }
+
+  post(path: string) {
+    return new TypedRouteHandler(path, 'post');
+  }
+
+  put(path: string) {
+    return new TypedRouteHandler(path, 'put');
+  }
+
+  delete(path: string) {
+    return new TypedRouteHandler(path, 'delete');
+  }
+}
+
+export class TypedRouteHandler<
+  RouteQuery extends z.ZodTypeAny,
+  RouteParams extends z.ZodTypeAny,
+  RouteBody extends z.ZodTypeAny
+> {
+  private schema: {
+    query?: z.ZodTypeAny;
+    params?: z.ZodTypeAny;
+    body?: z.ZodTypeAny;
+  } = {};
+
+  constructor(private readonly path: string, private readonly method: string) {}
+
+  query<Query extends z.ZodTypeAny>(schema: Query) {
+    this.schema.query = schema;
+    return this as unknown as TypedRouteHandler<Query, RouteParams, RouteBody>;
+  }
+
+  params<Params extends z.ZodTypeAny>(schema: Params) {
+    this.schema.params = schema;
+    return this as unknown as TypedRouteHandler<RouteQuery, Params, RouteBody>;
+  }
+
+  body<Body extends z.ZodTypeAny>(schema: Body) {
+    this.schema.body = schema;
+    return this as unknown as TypedRouteHandler<RouteQuery, RouteParams, Body>;
+  }
+
+  handler(handler: TypedHandle<RouteQuery, RouteParams, RouteBody>): HandlerMetadata {
+    const invokeHandler = async (req: Request, res: Response) => {
+      let message = '', query, params, body
+      try {
+        message = 'Query';
+        query = this.schema.query ? this.schema.query.parse(req.query) : undefined;
+        message = 'Params';
+        params = this.schema.params ? this.schema.params.parse(req.params) : undefined;
+        message = 'Body';
+        body = this.schema.body ? this.schema.body.parse(req.body) : undefined;
+      } catch (error: unknown) {
+        if (error instanceof z.ZodError) {
+          const validationError = fromZodError(error);
+          throw new ValidationError(`${message} ${validationError.toString()}`);
+        }
+      }
+      return handler({ query, params, body, req, res });
+    }
+    return {
+      method: this.method,
+      path: this.path,
+      handler: invokeHandler,
+      __handlerMetadata: true
+    }
+  }
+}
